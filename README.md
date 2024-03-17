a92766996e12e5e7787799ef81b0254bc04cd05b
BinoManjesh <binomanjesh@gmail.com>
2024-03-17 11:31:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Resume Maker
+
+A resume maker app built using React
+
+This is my submission to [Project: CV Application](https://www.theodinproject.com/lessons/node-path-react-new-cv-application) from [The Odin Project](https://www.theodinproject.com)
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import EducationForm from "./components/form/Education.jsx";
+import ExperienceForm from "./components/form/Experience.jsx";
+import GeneralInfoForm from "./components/form/GeneralInfo.jsx";
+
+function App() {
+  return (
+    <>
+      <GeneralInfoForm />
+      <EducationForm />
+      <ExperienceForm />
+    </>
+  );
+}
+
+export default App;
