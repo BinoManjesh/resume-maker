6ae384f67676dc4262251df0dadbafd29f3fa410
BinoManjesh <binomanjesh@gmail.com>
2024-03-17 13:04:37 +0530

    Add Education form

--- src/components/form/Education.jsx
+++ src/components/form/Education.jsx
@@ -1,3 +1,32 @@
-function Education() {}
+import Form from "./Form";
+import Input from "./Input";
+import { useState } from "react";
+
+function Education() {
+  const [school, setSchool] = useState("");
+  const [title, setTitle] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  return (
+    <Form heading="Education Details">
+      <ul>
+        <Input name="School name" value={school} setValue={setSchool} />
+        <Input name="Title of study" value={title} setValue={setTitle} />
+        <Input
+          name="Start date"
+          type="date"
+          value={startDate}
+          setValue={setStartDate}
+        />
+        <Input
+          name="End date"
+          type="date"
+          value={endDate}
+          setValue={setEndDate}
+        />
+      </ul>
+    </Form>
+  );
+}
 
 export default Education;
