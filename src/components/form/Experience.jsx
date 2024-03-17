4a9fc1c66e6e72373156a57b0bb09284256d123c
BinoManjesh <binomanjesh@gmail.com>
2024-03-17 13:17:18 +0530

    Add experience form

--- src/components/form/Experience.jsx
+++ src/components/form/Experience.jsx
@@ -1,3 +1,39 @@
-function Experience() {}
+import Form from "./Form";
+import Input from "./Input";
+import { useState } from "react";
+
+function Experience() {
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+  const [responsibilities, setResponsibilites] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  return (
+    <Form heading="Experience">
+      <ul>
+        <Input name="Company name" value={name} setValue={setName} />
+        <Input name="Position title" value={title} setValue={setTitle} />
+        <Input
+          name="Main responsibilities"
+          type="textarea"
+          value={responsibilities}
+          setValue={setResponsibilites}
+        />
+        <Input
+          name="Start date"
+          type="date"
+          value={startDate}
+          setValue={setStartDate}
+        />
+        <Input
+          name="End date"
+          type="date"
+          value={endDate}
+          setValue={setEndDate}
+        />
+      </ul>
+    </Form>
+  );
+}
 
 export default Experience;
--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,18 +1,23 @@
 import "../../styles/input.css";
 
 function Input({ name, id = name, type = "text", value, setValue }) {
+  const inputProps = {
+    id: id,
+    value: value,
+    type: type,
+    onChange: (e) => {
+      setValue(e.target.value);
+    },
+  };
   return (
     <>
       <li className="input-field">
         <label htmlFor={id}>{name}</label>
-        <input
-          id={id}
-          value={value}
-          type={type}
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-        />
+        {type == "textarea" ? (
+          <textarea {...inputProps} />
+        ) : (
+          <input {...inputProps} />
+        )}
       </li>
     </>
   );
